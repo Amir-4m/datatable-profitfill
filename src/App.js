74b6711ab2d1a240e9359ba09e9d2eac23ed0ff9
Amir Amiri <amiriamirr533@gmail.com>
2023-09-10 01:26:30 -0600

    finalizing components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
-import { useEffect } from "react";
-import { useDispatch } from 'react-redux';
 import DataTable from "./routes/data-table/data-table.component";
 import Navigation from "./routes/navigation/navigation.component";
 
 const App = () => {
-  const dispatch = useDispatch();
 
   return (
     < Routes >
--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
 import './index.scss';
 import App from './App';
-import { store, persistor } from './store/store';
-import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+    <React.StrictMode>
         <BrowserRouter>
-          <App />
+            <App/>
         </BrowserRouter>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
